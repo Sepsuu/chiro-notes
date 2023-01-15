e3b73995147cbb43dc3c40c40d1da76576fda6fd
Severi Peltonen <severi.peltonen@gmail.com>
2023-01-15 14:56:56 +0200

    component import

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminLayoutRoutingModule } from './admin-layout-routing.module';
-import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
+import { AdminLayoutComponent } from './admin-layout.component';
 
 
 @NgModule({
